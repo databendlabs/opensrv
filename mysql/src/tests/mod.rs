a69916132a7010718b2f275d2ce00379a1d18da0
Chojan Shang <psiace@outlook.com>
2022-03-09 16:19:27 +0800

    feat(mysql): datafuse-extras/msql-srv -> opensrv-mysql

---
+++ mysql/src/tests/mod.rs
@@ -0,0 +1,3 @@
+mod commands;
+mod packet;
+mod value;
---
+++ mysql/src/writers.rs
@@ -0,0 +1,150 @@
+use std::io::{self, Write};
+
+use byteorder::{LittleEndian, WriteBytesExt};
+
+use crate::myc::constants::{CapabilityFlags, StatusFlags};
+use crate::myc::io::WriteMysqlExt;
+use crate::packet::PacketWriter;
+use crate::{Column, ErrorKind, OkResponse};
+
+pub(crate) fn write_eof_packet<W: Write>(
+    w: &mut PacketWriter<W>,
+    s: StatusFlags,
+) -> io::Result<()> {
+    w.write_all(&[0xFE, 0x00, 0x00])?;
+    w.write_u16::<LittleEndian>(s.bits())?;
+    w.end_packet()
+}
+
+pub(crate) fn write_ok_packet<W: Write>(
+    w: &mut PacketWriter<W>,
+    client_capabilities: CapabilityFlags,
+    ok_packet: OkResponse,
+) -> io::Result<()> {
+    w.write_u8(ok_packet.header)?; // OK packet type
+    w.write_lenenc_int(ok_packet.affected_rows)?;
+    w.write_lenenc_int(ok_packet.last_insert_id)?;
+    if client_capabilities.contains(CapabilityFlags::CLIENT_PROTOCOL_41) {
+        w.write_u16::<LittleEndian>(ok_packet.status_flags.bits())?;
+        w.write_all(&[0x00, 0x00])?; // no warnings
+    } else if client_capabilities.contains(CapabilityFlags::CLIENT_TRANSACTIONS) {
+        w.write_u16::<LittleEndian>(ok_packet.status_flags.bits())?;
+    }
+
+    if client_capabilities.contains(CapabilityFlags::CLIENT_SESSION_TRACK) {
+        w.write_lenenc_str(ok_packet.info.as_bytes())?;
+        if ok_packet
+            .status_flags
+            .contains(StatusFlags::SERVER_SESSION_STATE_CHANGED)
+        {
+            w.write_lenenc_str(ok_packet.session_state_info.as_bytes())?;
+        }
+    } else {
+        w.write_all(ok_packet.info.as_bytes())?;
+    }
+    w.end_packet()
+}
+
+pub fn write_err<W: Write>(err: ErrorKind, msg: &[u8], w: &mut PacketWriter<W>) -> io::Result<()> {
+    w.write_u8(0xFF)?;
+    w.write_u16::<LittleEndian>(err as u16)?;
+    w.write_u8(b'#')?;
+    w.write_all(err.sqlstate())?;
+    w.write_all(msg)?;
+    w.end_packet()
+}
+
+use std::borrow::Borrow;
+
+pub(crate) fn write_prepare_ok<'a, PI, CI, W>(
+    id: u32,
+    params: PI,
+    columns: CI,
+    w: &mut PacketWriter<W>,
+    client_capabilities: CapabilityFlags,
+) -> io::Result<()>
+where
+    PI: IntoIterator<Item = &'a Column>,
+    CI: IntoIterator<Item = &'a Column>,
+    <PI as IntoIterator>::IntoIter: ExactSizeIterator,
+    <CI as IntoIterator>::IntoIter: ExactSizeIterator,
+    W: Write,
+{
+    let pi = params.into_iter();
+    let ci = columns.into_iter();
+
+    // first, write out COM_STMT_PREPARE_OK
+    w.write_u8(0x00)?;
+    w.write_u32::<LittleEndian>(id)?;
+    w.write_u16::<LittleEndian>(ci.len() as u16)?;
+    w.write_u16::<LittleEndian>(pi.len() as u16)?;
+    w.write_u8(0x00)?;
+    w.write_u16::<LittleEndian>(0)?; // number of warnings
+    w.end_packet()?;
+
+    if pi.len() > 0 {
+        write_column_definitions_41(pi, w, client_capabilities, false)?;
+    }
+    if ci.len() > 0 {
+        write_column_definitions_41(ci, w, client_capabilities, false)?;
+    }
+    Ok(())
+}
+
+/// works for Protocol::ColumnDefinition41 is set
+/// see: https://dev.mysql.com/doc/dev/mysql-server/latest/page_protocol_com_query_response_text_resultset_column_definition.html
+pub(crate) fn write_column_definitions_41<'a, I, W>(
+    i: I,
+    w: &mut PacketWriter<W>,
+    client_capabilities: CapabilityFlags,
+    is_com_field_list: bool,
+) -> io::Result<()>
+where
+    I: IntoIterator<Item = &'a Column>,
+    W: Write,
+{
+    for c in i {
+        let c = c.borrow();
+        use crate::myc::constants::UTF8_GENERAL_CI;
+        w.write_lenenc_str(b"def")?;
+        w.write_lenenc_str(b"")?;
+        w.write_lenenc_str(c.table.as_bytes())?;
+        w.write_lenenc_str(b"")?;
+        w.write_lenenc_str(c.column.as_bytes())?;
+        w.write_lenenc_str(b"")?;
+        w.write_lenenc_int(0xC)?;
+        w.write_u16::<LittleEndian>(UTF8_GENERAL_CI)?;
+        w.write_u32::<LittleEndian>(1024)?;
+        w.write_u8(c.coltype as u8)?;
+        w.write_u16::<LittleEndian>(c.colflags.bits())?;
+        w.write_all(&[0x00])?; // decimals
+        w.write_all(&[0x00, 0x00])?; // unused
+
+        if is_com_field_list {
+            w.write_all(&[0xfb])?;
+        }
+        w.end_packet()?;
+    }
+
+    if !client_capabilities.contains(CapabilityFlags::CLIENT_DEPRECATE_EOF) {
+        write_eof_packet(w, StatusFlags::empty())
+    } else {
+        Ok(())
+    }
+}
+
+pub(crate) fn column_definitions<'a, I, W>(
+    i: I,
+    w: &mut PacketWriter<W>,
+    client_capabilities: CapabilityFlags,
+) -> io::Result<()>
+where
+    I: IntoIterator<Item = &'a Column>,
+    <I as IntoIterator>::IntoIter: ExactSizeIterator,
+    W: Write,
+{
+    let i = i.into_iter();
+    w.write_lenenc_int(i.len() as u64)?;
+    w.end_packet()?;
+    write_column_definitions_41(i, w, client_capabilities, false)
+}
